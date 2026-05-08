3a665f553b56491e46ad9e8a44a9d53fcfb207d3
Cesar-Man <nikolin791@gmail.com>
2026-05-08 11:01:12 -0300

    Add files via upload

---
+++ Aula.py
@@ -0,0 +1,13 @@
+#Circunferencia de um circulo, area e perimetro
+
+import math
+
+Raio = float(input("Solta a letra quero o raio \[T]/   " ))
+
+perimetro = 2 * math.pi * Raio
+area  = math.pi * Raio ** 2
+print("Area : %.3f, pereimetro : %.3f" % (area , perimetro))
+
+
+
+
